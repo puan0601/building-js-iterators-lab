5f8c4f462788fcddf9bbd54405080be585dd57dc
puan0601 <anton.pugach@gmail.com>
2017-01-31 02:06:35 -0800

    almost finished myReduce 100%

--- exercises/myReduce.js
+++ exercises/myReduce.js
@@ -1,10 +1,14 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
 
 //  CODE INSIDE HERE   //
-
+    var value = initialValue || arr[0];
+    for (var i = 1; i < arr.length; i++) {
+      value = callback(value, arr[i], i, arr);
+    }
+  return value;
 }
 
 /*
